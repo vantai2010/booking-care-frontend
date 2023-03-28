06c9c06a0b01fb11647c0c4ca2d9843a9b3b4502
santaryot <chuvantai2002@gmail.com>
2023-03-28 11:10:37 +0700

    sua url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,100 +1,102 @@
 import axios from 'axios';
+require('dotenv').config()
+const URL_BACK_END = process.env.REACT_APP_BACKEND_URL
 
 let handleLoginService = (email, password) => {
-    return axios.post('http://localhost:5000/api/auth/login', { email, password })
+    return axios.post(`${URL_BACK_END}/api/auth/login`, { email, password })
 }
 
 let getAllUsersService = (id) => {
-    return axios.get(`http://localhost:5000/api/get-all-users/${id}`)
+    return axios.get(`${URL_BACK_END}/api/get-all-users/${id}`)
 }
 
 let createNewUserService = (data) => {
-    return axios.post('http://localhost:5000/api/create-new-user', data)
+    return axios.post(`${URL_BACK_END}/api/create-new-user`, data)
 }
 
 let deleteUserService = (id) => {
-    return axios.delete(`http://localhost:5000/api/delete-user?id=${id}`)
+    return axios.delete(`${URL_BACK_END}/api/delete-user?id=${id}`)
 }
 
 let updateUserService = (data) => {
-    return axios.put(`http://localhost:5000/api/update-user?id=${data.id}`, data)
+    return axios.put(`${URL_BACK_END}/api/update-user?id=${data.id}`, data)
 }
 
 let getAllCodeService = (type) => {
-    return axios.get(`http://localhost:5000/api/allcode?type=${type}`)
+    return axios.get(`${URL_BACK_END}/api/allcode?type=${type}`)
 }
 
 let getTopDoctorService = (role) => {
-    return axios.get(`http://localhost:5000/api/top-doctor-home?role=${role}`)
+    return axios.get(`${URL_BACK_END}/api/top-doctor-home?role=${role}`)
 }
 
 let getAllDoctorsService = () => {
-    return axios.get('http://localhost:5000/api/get-all-doctors')
+    return axios.get(`${URL_BACK_END}/api/get-all-doctors`)
 }
 
 let saveInforDoctorService = (data) => {
-    return axios.post('http://localhost:5000/api/save-infor-doctor', data)
+    return axios.post(`${URL_BACK_END}/api/save-infor-doctor`, data)
 }
 
 let getDetailDoctorService = (id) => {
-    return axios.get(`http://localhost:5000/api/get-detail-doctor-by-id?id=${id}`)
+    return axios.get(`${URL_BACK_END}/api/get-detail-doctor-by-id?id=${id}`)
 }
 
 let saveBulkScheduleDoctorService = (data) => {
-    return axios.post('http://localhost:5000/api/bulk-create-schedule', data)
+    return axios.post(`${URL_BACK_END}/api/bulk-create-schedule`, data)
 }
 
 let getScheduleDoctorByDateService = (data) => {
-    return axios.get(`http://localhost:5000/api/get-schedule-doctor-by-date?doctorId=${data.doctorId}&date=${data.date}`)
+    return axios.get(`${URL_BACK_END}/api/get-schedule-doctor-by-date?doctorId=${data.doctorId}&date=${data.date}`)
 }
 
 let getInforDoctorById = (id) => {
-    return axios.get(`http://localhost:5000/api/get-infor-doctor-by-id?id=${id}`)
+    return axios.get(`${URL_BACK_END}/api/get-infor-doctor-by-id?id=${id}`)
 }
 
 let getProfileDoctorById = (id) => {
-    return axios.get(`http://localhost:5000/api/get-profile-doctor-by-id?id=${id}`)
+    return axios.get(`${URL_BACK_END}/api/get-profile-doctor-by-id?id=${id}`)
 }
 
 let postBookAppointment = (data) => {
-    return axios.post('http://localhost:5000/api/patient-book-appoinment', data)
+    return axios.post(`${URL_BACK_END}/api/patient-book-appoinment`, data)
 }
 
 let postVerifyBookAppointment = (data) => {
-    return axios.post('http://localhost:5000/api/verify-book-appoinment', data)
+    return axios.post(`${URL_BACK_END}/api/verify-book-appoinment`, data)
 }
 
 let createNewSpecialtyService = (data) => {
-    return axios.post('http://localhost:5000/api/create-new-specialty', data)
+    return axios.post(`${URL_BACK_END}/api/create-new-specialty`, data)
 }
 
 let createNewClinicService = (data) => {
-    return axios.post('http://localhost:5000/api/create-new-clinic', data)
+    return axios.post(`${URL_BACK_END}/api/create-new-clinic`, data)
 }
 
 let getAllSpecialtyService = () => {
-    return axios.get('http://localhost:5000/api/get-all-specialty')
+    return axios.get(`${URL_BACK_END}/api/get-all-specialty`)
 }
 
 let getDetailSpecialtyByIdService = (id, location) => {
-    return axios.get(`http://localhost:5000/api/get-detail-specialty-by-id?id=${id}&location=${location}`)
+    return axios.get(`${URL_BACK_END}/api/get-detail-specialty-by-id?id=${id}&location=${location}`)
 }
 
 let getAllClinicService = () => {
-    return axios.get('http://localhost:5000/api/get-all-clinic')
+    return axios.get(`${URL_BACK_END}/api/get-all-clinic`)
 }
 
 
 let getDetailClinicByIdService = (id) => {
-    return axios.get(`http://localhost:5000/api/get-detail-by-id-clinic?id=${id}`)
+    return axios.get(`${URL_BACK_END}/api/get-detail-by-id-clinic?id=${id}`)
 }
 
 let getAllPatientForDoctorService = (data) => {
-    return axios.get(`http://localhost:5000/api/get-list-patient?doctorId=${data.doctorId}&date=${data.date}`)
+    return axios.get(`${URL_BACK_END}/api/get-list-patient?doctorId=${data.doctorId}&date=${data.date}`)
 }
 
 let sendRemedyService = (data) => {
-    return axios.post('http://localhost:5000/api/send-remedy', data)
+    return axios.post(`${URL_BACK_END}/api/send-remedy`, data)
 }
 
 export {
